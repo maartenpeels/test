61bdf3bbafad1690739a0accb85c264640abc2c6
Youri van Mill <yourivanmill@gmail.com>
2018-04-04 10:51:46 +0200

    Test to test

---
+++ src/test/java/com/yourivanmill/SomeBeanTest.java
@@ -0,0 +1,13 @@
+package com.yourivanmill;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SomeBeanTest {
+
+    @Test
+    public void TestAssertTrue() {
+        assertTrue(true);
+    }
+}
